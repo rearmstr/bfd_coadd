14c6777eb114f79bc16d2509eba26b2378213033
Bob Armstrong <rearmstr@gmail.com>
2017-07-18 10:47:08 -0500

    fix weight function argument

--- bin/generate_moments.py
+++ bin/generate_moments.py
@@ -32,7 +32,7 @@ args = parser.parse_args()
 
 
 seed=int(np.random.rand()*100000000)
-weight = bfd.KSigmaWeight(args.ngal,args.weight_sigma)
+weight = bfd.KSigmaWeight(args.weight_n,args.weight_sigma)
 
 table_multi = None
 table_coadd = None
